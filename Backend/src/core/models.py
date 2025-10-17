0b6211dd4db52977e566f1738fdb1510d2e416fc
purpleswtr <sergeenko.mihail@yandex.ru>
2025-10-17 18:35:55 +0300

    update-logics

--- Backend/src/core/models.py
+++ Backend/src/core/models.py
@@ -16,9 +16,6 @@ class GameSession(BaseModel):
     markers: dict[UUID, str] = {}
     status: str = "waiting"
     
-    # Реализовать: 
-    # переход хода
-
     async def choose_img(self, user_id: str, marker_type: str) -> None:
         self.markers[user_id] = marker_type
 
@@ -61,7 +58,7 @@ class GameSession(BaseModel):
             return False
         else: return True
 
-    def change_turn(self, user_id: UUID):
+    def change_turn(self):
         players = list(self.players.keys())
         if not players:
             return None
@@ -73,24 +70,76 @@ class GameSession(BaseModel):
             self.current_turn = players[next_index]
         
         return self.current_turn
+    
+    def action_move(self, user_id: UUID, row: int, col: int) -> bool:
+
+        if user_id != self.current_turn:
+            return False
+        if self.field[row][col] is not None:
+            return False
+
+        self.field[row][col] =self.markers[user_id]
+
+        if self.win_check():
+            self.status = "finished"
+            self.winner_player = user_id
+        elif self.draw_check():
+            self.status = "finished"
+        ...
+    def action_exit(self, user_id: UUID):
+        self.players[user_id] = None
+        ...
+
+    def dev_draw_field(self):
+        for row in range(3):
+            line = ""
+            for col in range(3):
+                cell = self.field[row][col]
+                if cell is None:
+                    line += " - "
+                else:
+                    line += f" {cell} "
+            print(line)
+
 active_sessions: dict[str, GameSession] = {}
 
 gs = GameSession(session_id="1")
 
+
+
 p1 = Player(name="jonh")
 p2 = Player(name="bob")
+
+gs.markers[p1.id] = "0"
+gs.markers[p2.id] = "X"
+
 
 gs.players[p1.id] = p1
 gs.players[p2.id] = p2
 
-gs.change_turn(user_id=p1.id)
-print(gs.current_turn)
-gs.change_turn(user_id=p1.id)
-print(gs.current_turn)
-gs.change_turn(user_id=p1.id)
-print(gs.current_turn)
-gs.change_turn(user_id=p1.id)
-print(gs.current_turn)
+gs.current_turn = p1.id
+
+while gs.status == "waiting":
+    r = input()
+    c = input()
+    gs.action_move(user_id=gs.current_turn, row=int(r), col=int(c))
+    gs.change_turn()
+
+    gs.dev_draw_field()
+
+print(gs.status)
+
+print(gs.winner_player)
+
+
+# gs.change_turn(user_id=p1.id)
+# print(gs.current_turn)
+# gs.change_turn(user_id=p1.id)
+# print(gs.current_turn)
+# gs.change_turn(user_id=p1.id)
+# print(gs.current_turn)
+# gs.change_turn(user_id=p1.id)
+# print(gs.current_turn)
 
 # gs.field=[
 #     ["1","0","1"],
