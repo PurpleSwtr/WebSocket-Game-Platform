6b1d4918e917fb42ee0da5de3dfa8e67df263ef2
purpleswtr <sergeenko.mihail@yandex.ru>
2025-10-17 15:14:09 +0300

    add-win-draw-change-turn-logics

--- Backend/src/core/models.py
+++ Backend/src/core/models.py
@@ -15,10 +15,8 @@ class GameSession(BaseModel):
     winner_player: UUID = None
     markers: dict[UUID, str] = {}
     status: str = "waiting"
-
+    
     # Реализовать: 
-    # проверку на выигрыш
-    # проверку на ничью
     # переход хода
 
     async def choose_img(self, user_id: str, marker_type: str) -> None:
@@ -44,22 +42,63 @@ class GameSession(BaseModel):
             [(0,1),(1,1),(2,1)],
             [(0,2),(1,2),(2,2)],
         ]
-        # for comb in win_combos:
-        #     try_combination = []
-        #     for coordinates in comb:
-        #         try_combination.append(self.field[coordinates[0]][coordinates[1]])
-        #         if len(try_combination) > 2:
-        #             normalize_combinations = set(try_combination)
-        #             if len(try_combination) == len(normalize_combinations):
-        #                 return True
 
-gs = GameSession(session_id="1") 
-gs.field=[
-    [None,"1",None],
-    [None,"1",None],
-    [None,"1",None]
-    ]
-check = gs.win_check()
-print(check)
+        for win_comb in win_combos:
+            symbols = []
+            for r, c in win_comb:
+                symb = self.field[c][r]
+                symbols.append(symb)
+            if len(set(symbols)) == 1 and not None in set(symbols):
+                return True
+        return False
     
+    def draw_check(self):
+        for col in range(3):
+            cells = []
+            for cell in self.field[col]:
+                cells.append(cell)
+        if None in cells:
+            return False
+        else: return True
+
+    def change_turn(self, user_id: UUID):
+        players = list(self.players.keys())
+        if not players:
+            return None
+        if self.current_turn is None:
+            self.current_turn = players[0]
+        else:
+            current_index = players.index(self.current_turn)
+            next_index = (current_index + 1) % len(players)
+            self.current_turn = players[next_index]
+        
+        return self.current_turn
 active_sessions: dict[str, GameSession] = {}
+
+gs = GameSession(session_id="1")
+
+p1 = Player(name="jonh")
+p2 = Player(name="bob")
+
+gs.players[p1.id] = p1
+gs.players[p2.id] = p2
+
+gs.change_turn(user_id=p1.id)
+print(gs.current_turn)
+gs.change_turn(user_id=p1.id)
+print(gs.current_turn)
+gs.change_turn(user_id=p1.id)
+print(gs.current_turn)
+gs.change_turn(user_id=p1.id)
+print(gs.current_turn)
+
+# gs.field=[
+#     ["1","0","1"],
+#     ["1","1","1"],
+#     ["0",None,"0"]
+#     ]
+# draw_check = gs.draw_check()
+# win_check = gs.win_check()
+    
+# print(win_check)
+# print(draw_check)
